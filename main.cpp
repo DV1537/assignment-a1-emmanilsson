51ecdbef4d1a214029623e0f5917101fa2e87126
emmanilsson <eemmni03@gmail.com>
2018-11-17 12:20:32 +0100

    Assignment A1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,77 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
-/**
- * The program reads numbers from a file and prints the sum of them
- * Example: main.exe ./tests/input.in
- * @param  argc [description]
- * @param  argv [description]
- * @return      [description]
- */
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
-    //I was here
-    int a = 0;
-    int sum = 0;
-    
-    std::ifstream myReadFile;
-    
-    myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
+
+    std::ifstream myfile;
+
+    myfile.open("textfile.txt");
+
+    double *ptr; //Deklarera en pekare av typen double
+    double average = 0, sum = 0;
+    int numberOfElements, n;
+    ptr = new double[numberOfElements]; //Pekaren pekar på en dynamiskt allokerad array som har numberOfElements antal element som användaren får mata in
+
+    std::cout << "How many numbers did you write in the file?";
+    std::cin >> numberOfElements;
+
+
+
+    for (n = 0; n < numberOfElements; n++)
     {
-        sum += a;
+        myfile >> ptr[n];                                       //Läser in alla värden från filen
+
+
+        if (myfile.fail())                                      //Om det sker felinmatningar
+        {
+            std::cout << "Wrong input, program terminates...\n"
+                      << "Press [ENTER] to close program";
+            getchar();
+            getchar();
+
+            exit(EXIT_FAILURE);                                 //Skall programmet stängas ner
+        }
+
     }
-    myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
+
+
+
+    std::cout << "You have entered: ";
+    for (n = 0; n < numberOfElements; n++)
+    {
+        std::cout << std::fixed << std::setprecision(3) << ptr[n] << ", ";
+    }
+
+
+
+    for (n = 0; n < numberOfElements; n++)
+    {
+        sum += ptr[n];
+    }
+
+    getchar();
+    getchar();
+
+
+    myfile.close();
+
+
+
+    average = sum / numberOfElements;
+
+    std::cout << "The number/s greater than the average is: ";
+    for (n = 0; n < numberOfElements; n++)
+    {
+        if (ptr[n] > average)
+            std::cout << ptr[n] << " ";
+    }
+
+
+
+    getchar();
+    getchar();
+
     return 0;
 }
-
